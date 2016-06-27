37f548c43316dd7d3b7ed4b75b87580748deb31a
Rem Zolotykh <rem@zolotykh.net>
2016-06-27 17:13:22 +0200

    Sign up form and its state

---
+++ client/components/signup/SignupForm.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import timezones from '../../data/timezones';
+import map from 'lodash/map';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      timezone: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    const options = map(timezones, (val, key) =>
+      <option key={val} value={val}>{key}</option>
+    );
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
+
+        <div className="form-group">
+          <label className="control-label">Username</label>
+          <input
+            value={this.state.username}
+            onChange={this.onChange}
+            type="text"
+            name="username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.email}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.password}
+            type="password"
+            name="password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password Confirmation</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.passwordConfirmation}
+            type="password"
+            name="passwordConfirmation"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Timezone</label>
+          <select
+            className="form-control"
+            name="timezone"
+            onChange={this.onChange}
+            value={this.state.timezone}
+          >
+            <option value="" disabled>Choose Your Timezone</option>
+            {options}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import SignupForm from './SignupForm';
 
 class SignupPage extends React.Component {
   render() {
     return (
-      <h1>Sign up page</h1>
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <SignupForm />
+        </div>
+      </div>
     );
   }
 }
